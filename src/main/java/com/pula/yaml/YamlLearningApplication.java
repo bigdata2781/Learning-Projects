08ff53f4203e85b6994290f0629f5723fa59ef89
Naveen <naveen.gupta.27@gmail.com>
2021-10-21 17:19:00 +0530

    Initialize yaml project

---
+++ src/main/java/com/pula/yaml/YamlLearningApplication.java
@@ -0,0 +1,13 @@
+package com.pula.yaml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YamlLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YamlLearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pula/yaml/YamlLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pula.yaml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YamlLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
