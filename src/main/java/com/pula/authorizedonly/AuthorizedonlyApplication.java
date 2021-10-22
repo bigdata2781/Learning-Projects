f928bc7dc42b3fca7c865e90fd3a6593a62845fa
Naveen <naveen.gupta.27@gmail.com>
2021-10-22 23:19:56 +0530

    Initialized Authoriz Only

---
+++ src/main/java/com/pula/authorizedonly/AuthorizedonlyApplication.java
@@ -0,0 +1,13 @@
+package com.pula.authorizedonly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorizedonlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorizedonlyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pula/authorizedonly/AuthorizedonlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pula.authorizedonly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizedonlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
